a524e7310f24c6e1e6bcf5224235ef17c82dc294
Temesgen <abuadanu89@gmail.com>
2022-12-10 04:17:25 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,6 @@
-able File  14 lines (13 sloc)  302 Bytes
-
 #include <stdio.h>
-
-/**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-gcc -wall 5-printf.c -o 5-printf
