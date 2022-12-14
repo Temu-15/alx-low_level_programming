7265b6a1d47b4e8b064f2af40556a9ff3657068f
Temesgen <abuadanu89@gmail.com>
2022-12-14 09:08:41 -0800

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+# include "main.h"
+/**
+ * _islower : checks whether a character is small
+ *
+ * Return : Always 0.
+ */
+int _islower(int c)
+{
+	int i;
+
+	for (i = 65 ; i < 123 ; i++)
+	{
+		if (i == c)
+		{
+			break;
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	return (0);
+}
