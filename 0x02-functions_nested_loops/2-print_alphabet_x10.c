1edd3923e6f09e450c3208aefbe597ab219fcbb9
Temesgen <abuadanu89@gmail.com>
2022-12-14 04:55:48 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,19 @@
 # include "main.h"
 /**
- * print_alphabet_x10 : prints the alphabet ten times 
+ * print_alphabet_x10 - prints alphabet
  *
+ * Description : prints every letter of english
  * Return : Always 0.
  */
 void print_alphabet_x10(void)
 {
 	char ch[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		int j;
+
 		for (j = 0 ; j < 26 ; j++)
 		{
 			_putchar(ch[j]);
