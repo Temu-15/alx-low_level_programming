f8098ad4c73e791d9db27e279ba49ed4b8ed0250
Temesgen <abuadanu89@gmail.com>
2022-12-14 12:46:46 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+# include "main.h"
+/**
+ * print_sign - prnts the sign of a number
+ * @n: takes input as an integer
+ *
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	char ch[] = "+";
+	char neg[] = "-"
+	if (n > 0)
+	{
+		_putchar(ch[]);
+		return (1);
+	}
+	if else (n == 0)
+	{ 
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar(neg[]);
+		return (-1);
+	}
+	return (0);
+}
