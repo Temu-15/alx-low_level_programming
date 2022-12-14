d8824f54479f6fb42160b2cf885be58ba7647955
Temesgen <abuadanu89@gmail.com>
2022-12-14 03:00:42 -0800

    0. _putchar mandatory

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,19 @@
 /**
  *  * main - prints _putchar, fllowd by a new linees
  *   * Return: Alwys 0 (Success)
- *    */
+ *    
+ */
 int main(void)
 {
-		char abe[] = "_putchar";
+	char ch[] = "_putchar";
+	
+	int c;
 
-			int c;
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ch[c]);
+	}
 
-				for (c = 0; c < 8; c++)
-						{
-									_putchar(abe[c]);
-										}
-					_putchar('\n');
-						return (0);
+	_putchar('\n');
+	return (0);
 }
