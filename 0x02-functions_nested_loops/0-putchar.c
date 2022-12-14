4dce95389874bbc7007ec6f11ddbe4c615287398
Temesgen <abuadanu89@gmail.com>
2022-12-14 03:09:09 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- *  * main - prints _putchar, fllowd by a new linees
- *   * Return: Alwys 0 (Success)
- *    
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
 	char ch[] = "_putchar";
-	
 	int c;
 
 	for (c = 0; c < 8; c++)
